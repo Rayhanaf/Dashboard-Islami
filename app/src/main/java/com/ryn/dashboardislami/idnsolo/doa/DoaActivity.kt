e76ea6fa38c9a2fc58c81d01834072b231afd41c
Rayhanaf <rayhan301109@gmail.com>
2023-10-11 09:18:05 +0700

    DI-Data Doa New Update 10/11/2023

--- app/src/main/java/com/ryn/dashboardislami/idnsolo/doa/DoaActivity.kt
+++ app/src/main/java/com/ryn/dashboardislami/idnsolo/doa/DoaActivity.kt
@@ -13,6 +13,8 @@ class DoaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDoaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val actionBar = supportActionBar
         actionBar?.title = "Doa Harian"
@@ -22,9 +24,42 @@ class DoaActivity : AppCompatActivity() {
     }
 
     private fun intentToListActivity() {
-        val intent  = Intent(this, ListDoaActivity::class.java)
-        intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.pagi_dan_malam))
-        intent.putExtra(ListDoaActivity.EXTRA_IMAGE, getString(R.string.pagi_dan_malam)
+        binding.imgPagiMalam.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.pagi_dan_malam))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_pagi_malam)
+            startActivity(intent)
+        }
+        binding.imgRumah.setOnClickListener {
+            val intent  = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.rumah))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_rumah)
+            startActivity(intent)
+        }
+        binding.cardMakananMinuman.setOnClickListener {
+            val intent  = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.makanan_dan_minuman))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_makanan_minuman)
+            startActivity(intent)
+        }
+        binding.cardDoaPerjalanan.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.perjalanan))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_perjalanan)
+            startActivity(intent)
+        }
+        binding.cardEtikaBaik.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.etika_baik))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_etika_baik)
+            startActivity(intent)
+        }
+        binding.cardDoaShalat.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.shalat))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_sholat)
+            startActivity(intent)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/ryn/dashboardislami/idnsolo/doa/ListDoaActivity.kt
+++ app/src/main/java/com/ryn/dashboardislami/idnsolo/doa/ListDoaActivity.kt
@@ -1,21 +1,76 @@
 package com.ryn.dashboardislami.idnsolo.doa
 
+import android.icu.text.CaseMap.Title
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ryn.dashboardislami.idnsolo.R
 import com.ryn.dashboardislami.idnsolo.databinding.ActivityDetailListDoaBinding
+import com.ryn.dashboardislami.idnsolo.doa.Adapter.DoaListAdapter
+import com.ryn.dashboardislami.idnsolo.doa.data.DoaEtikaBaik
+import com.ryn.dashboardislami.idnsolo.doa.data.DoaMakananMinuman
+import com.ryn.dashboardislami.idnsolo.doa.data.DoaPagiMalam
+import com.ryn.dashboardislami.idnsolo.doa.data.DoaPerjalanan
+import com.ryn.dashboardislami.idnsolo.doa.data.DoaRumah
+import com.ryn.dashboardislami.idnsolo.doa.data.DoaShalat
+import com.ryn.dashboardislami.idnsolo.doa.model.DoaModel
 
 
 private lateinit var binding: ActivityDetailListDoaBinding
 
 class ListDoaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailListDoaBinding
+    lateinit var title: String
+    var logo: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailListDoaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        title = intent.getStringExtra(EXTRA_TITLE).toString()
+        logo = intent.getIntExtra(EXTRA_IMAGE, 0)
+
+        val actionBar = supportActionBar
+        actionBar?.title = intent.getStringExtra(EXTRA_TITLE)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        initRecyclerView()
+
+    }
+
+    private fun initRecyclerView() {
+        val list: ArrayList<DoaModel> = arrayListOf()
+        when (title) {
+            getString(R.string.pagi_dan_malam) -> {
+                list.addAll(DoaPagiMalam.listDoaPagiDanMalam)
+            }
+
+            getString(R.string.rumah) -> {
+                list.addAll(DoaRumah.listDoaRumah)
+            }
+            getString(R.string.makanan_dan_minuman) -> {
+                list.addAll(DoaMakananMinuman.listDoaMakananDanMinuman)
+            }
+            getString(R.string.perjalanan) -> {
+                list.addAll(DoaPerjalanan.listDoaPerjalanan)
+            }
+            getString(R.string.etika_baik) -> {
+                list.addAll(DoaEtikaBaik.listDoaEtikaBaik)
+            }
+            getString(R.string.shalat) -> {
+                list.addAll(DoaShalat.listDoaShalat)
+            }
+        }
+        binding.rvListDoa.hasFixedSize()
+        binding.rvListDoa.layoutManager = LinearLayoutManager(this)
+        val adapter = DoaListAdapter(list, title, logo)
+        binding.rvListDoa.adapter = adapter
     }
 
     companion object {
-        const val EXTRA_IMAGE = "extra_image"
         const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_IMAGE = "extra_image"
     }
 }
