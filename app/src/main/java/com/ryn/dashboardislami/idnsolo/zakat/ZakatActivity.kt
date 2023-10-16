3b8e647b11d38a478f00fa342f055761c01e9f51
Rayhanaf <rayhan301109@gmail.com>
2023-10-16 11:05:44 +0700

    DI-Data Doa New Update 10/16/2023

--- app/src/main/java/com/ryn/dashboardislami/idnsolo/zakat/ZakatActivity.kt
+++ app/src/main/java/com/ryn/dashboardislami/idnsolo/zakat/ZakatActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ryn.dashboardislami.idnsolo.R
 import com.ryn.dashboardislami.idnsolo.databinding.ActivityZakatBinding
+import java.text.NumberFormat
+import java.util.Locale
 
 class ZakatActivity : AppCompatActivity() {
 
@@ -13,31 +15,35 @@ class ZakatActivity : AppCompatActivity() {
         binding = ActivityZakatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val actionBar = supportActionBar
+        actionBar!!.title = "Zakat"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
         initHitungZakat()
     }
 
     private fun initHitungZakat() {
         binding.btnHitung.setOnClickListener {
-            val isEmptyField = false
+            var isEmptyField = false
             val totalHarta = binding.edtTotalHartaKesuluruhan.text.trim().toString()
+            val emas = 92395000
+            val persen = 0.025
+            val localeID = Locale("in", "ID")
+            val formatRupiah: NumberFormat = NumberFormat.getCurrencyInstance(localeID)
+
+            if(totalHarta.isEmpty()){
+                isEmptyField = true
+                binding.edtTotalHartaKesuluruhan.error = "Field ini tidak boleh kosong"
+            }
 
             if(!isEmptyField){
-                if(totalHarta.isEmpty()){
-                    !isEmptyField
-                    binding.edtTotalHartaKesuluruhan.error = "Field ini tidak boleh kosong"
-                }
 
-                if(!isEmptyField){
-                    val emas = 92395000
-                    val persen = 0.025
-
-                    if (totalHarta < emas.toString()){
-                        binding.tvStatus.text = "Anda Belum wajib"
-                    } else {
-                        val zakat = totalHarta.toDouble() * persen
-                        binding.tvStatus.text = "Anda wajib membayar zakat"
-                        binding.tvZakatYangDikeluarkan.text = "Rp. $zakat"
-                    }
+                if (totalHarta < emas.toString()){
+                    binding.tvStatus.text = "Anda Belum wajib"
+                } else {
+                    val zakat = totalHarta.toInt() * persen
+                    binding.tvStatus.text = "Anda wajib membayar zakat"
+                    binding.tvZakatYangDikeluarkan.text = formatRupiah.format(zakat)
                 }
             }
         }
